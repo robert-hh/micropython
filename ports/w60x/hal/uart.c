e1af6f79f4713ebe4f101cd4bf1b538b7673b300
robert <robert@hammelrath.com>
2020-04-15 15:30:29 +0200

    w600: Initial port for the W600 chip
    Based on the 1.10 port of WinnerMicro.
    This is just the first shot. File scopied over and cleaned two compile
    errors. It builds and runs. What is missing:
    - TESTING!!!
    - board definitions
    - manifest structure
    - inclusion of newer modules, e.g. uasyncio
    - ......

---
+++ ports/w60x/hal/uart.c
@@ -0,0 +1,120 @@
+﻿/*
+ * This file is part of the MicroPython project, http://micropython.org/
+ *
+ * Development of the code in this file was sponsored by Microbric Pty Ltd
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Damien P. George
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include "wm_include.h"
+#include "mphalport.h"
+
+#include "py/mpstate.h"
+#include "py/mphal.h"
+#include "py/stream.h"
+
+extern int sendchar(int ch);
+
+STATIC uint8_t stdin_ringbuf_array[256];
+ringbuf_t stdin_ringbuf = {stdin_ringbuf_array, sizeof(stdin_ringbuf_array)};
+
+uintptr_t mp_hal_stdio_poll(uintptr_t poll_flags) {
+    uintptr_t ret = 0;
+    if ((poll_flags & MP_STREAM_POLL_RD) && stdin_ringbuf.iget != stdin_ringbuf.iput) {
+        ret |= MP_STREAM_POLL_RD;
+    }
+    return ret;
+}
+
+// Receive single character
+int mp_hal_stdin_rx_chr(void) {
+    int c;
+    for (;;) {
+        c = ringbuf_get(&stdin_ringbuf);
+        if (c != -1) {
+            return c;
+        } else {
+            MICROPY_EVENT_POLL_HOOK
+            tls_os_time_delay(1);
+        }
+    }
+}
+
+// Send string of given length
+void mp_hal_stdout_tx_strn(const char *str, size_t len) {
+    while (len--) {
+        // wait for TXE
+        sendchar(*str++);
+    }
+}
+
+void mp_hal_stdout_tx_str(const char *str) {
+    mp_hal_stdout_tx_strn(str, strlen(str));
+}
+
+// Efficiently convert "\n" to "\r\n"
+void mp_hal_stdout_tx_strn_cooked(const char *str, size_t len) {
+    const char *last = str;
+    while (len--) {
+        if (*str == '\n') {
+            if (str > last) {
+                mp_hal_stdout_tx_strn(last, str - last);
+            }
+            mp_hal_stdout_tx_strn("\r\n", 2);
+            ++str;
+            last = str;
+        } else {
+            ++str;
+        }
+    }
+    if (str > last) {
+        mp_hal_stdout_tx_strn(last, str - last);
+    }
+}
+
+STATIC s16 uart_rx_callback(u16 len) {
+    int c;
+    while(tls_uart_read(TLS_UART_0, &c, 1) > 0) {
+        if (c == mp_interrupt_char) {
+            // inline version of mp_keyboard_interrupt();
+            MP_STATE_VM(mp_pending_exception) = MP_OBJ_FROM_PTR(&MP_STATE_VM(mp_kbd_exception));
+#if MICROPY_ENABLE_SCHEDULER
+            if (MP_STATE_VM(sched_state) == MP_SCHED_IDLE) {
+                MP_STATE_VM(sched_state) = MP_SCHED_PENDING;
+            }
+#endif
+        } else {
+            // this is an inline function so will be in IRAM
+            ringbuf_put(&stdin_ringbuf, c);
+        }
+    }
+}
+
+void uart_init(void) {
+    tls_uart_port_init(TLS_UART_0, NULL, 0);
+    tls_uart_rx_callback_register((u16) TLS_UART_0, uart_rx_callback);
+}
+
