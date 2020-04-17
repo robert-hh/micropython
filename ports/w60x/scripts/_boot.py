a181184b3b58351ac0f6a239c2833d6cf972e14d
robert <robert@hammelrath.com>
2020-04-17 15:43:14 +0200

    gccollect.c: Fix error of non inlcuding registers on the stack
    Thanks to @dhylands and @stijn this was fixed. The related changes
    are in gccollect.c and Makefile.

--- ports/w60x/scripts/_boot.py
+++ ports/w60x/scripts/_boot.py
@@ -12,9 +12,9 @@
 # print("    /_/    \/    \_\ ")
 
 from upysh import *
-from easyw600 import *
 try:
     import wifi_config
+    from easyw600 import *
     connect(wifi_config.WIFI_SSID, wifi_config.WIFI_PASSWD)
     ftpserver()
 except:
