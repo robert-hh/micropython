75bdcf82ec2b0d22c5d67cc4605472ff2d80f7b0
Philipp Ebensberger <philipp.ebensberger@3bricks-software.de>
2021-06-12 23:22:47 +0200

    mimxrt/sdcard: Implemented card init sequence.

--- ports/mimxrt/machine_sdcard.c
+++ ports/mimxrt/machine_sdcard.c
@@ -1,3 +1,4 @@
+
 /*
  * This file is part of the MicroPython project, http://micropython.org/
  *
@@ -29,28 +30,350 @@
 #include "fsl_iomuxc.h"
 #include "fsl_usdhc.h"
 
+#include "ticks.h"
 #include "modmachine.h"
 
 
 #define SDCARD_INIT_ARG_ID (0)
 
+#define SDMMC_VOLTAGE_WINDOW_SD            0x80100000U
+#define SDMMC_HIGH_CAPACITY                0x40000000U
+#define SD_SWITCH_1_8V_CAPACITY            ((uint32_t)0x01000000U)
+#define SDMMC_MAX_VOLT_TRIAL               ((uint32_t)0x0000FFFFU)
+
 
 typedef struct _machine_adc_obj_t {
     mp_obj_base_t base;
     USDHC_Type *sdcard;
+    uint32_t rca;
 } mimxrt_sdcard_obj_t;
 
+typedef struct _cid_t {
+    uint32_t cid[4];
+} cid_t;
 
-STATIC const mimxrt_sdcard_obj_t mimxrt_sdcard_objs[1] = {
+typedef struct _csd_t {
+    uint32_t csd[4];
+} csd_t;
+
+
+STATIC mimxrt_sdcard_obj_t mimxrt_sdcard_objs[1] = {
     {
         .base.type = &machine_sdcard_type,
         .sdcard = USDHC1,
+        .rca = 0x0UL,
     }
 };
 
 STATIC const mp_arg_t allowed_args[] = {
     [SDCARD_INIT_ARG_ID]     { MP_QSTR_id, MP_ARG_INT, {.u_int = 0} },
 };
+
+
+static void sdcard_check_status(status_t status, uint8_t command) {
+    if (status != kStatus_Success) {
+        mp_printf(&mp_plat_print, "Command %d failed!", command);
+        switch (status)
+        {
+            case (kStatus_USDHC_BusyTransferring): {
+                mp_printf(&mp_plat_print, " - BusyTransferring (%d)\n", status);
+                break;
+            }
+            case (kStatus_USDHC_PrepareAdmaDescriptorFailed): {
+                mp_printf(&mp_plat_print, " - PrepareAdmaDescriptorFailed (%d)\n", status);
+                break;
+            }
+            case (kStatus_USDHC_SendCommandFailed): {
+                mp_printf(&mp_plat_print, " - SendCommandFailed (%d)\n", status);
+                break;
+            }
+            case (kStatus_USDHC_TransferDataFailed): {
+                mp_printf(&mp_plat_print, " - TransferDataFailed (%d)\n", status);
+                break;
+            }
+            case (kStatus_USDHC_DMADataAddrNotAlign): {
+                mp_printf(&mp_plat_print, " - DMADataAddrNotAlign (%d)\n", status);
+                break;
+            }
+            case (kStatus_USDHC_ReTuningRequest): {
+                mp_printf(&mp_plat_print, " - ReTuningRequest (%d)\n", status);
+                break;
+            }
+            case (kStatus_USDHC_TuningError): {
+                mp_printf(&mp_plat_print, " - TuningError (%d)\n", status);
+                break;
+            }
+            case (kStatus_USDHC_NotSupport): {
+                mp_printf(&mp_plat_print, " - NotSupport (%d)\n", status);
+                break;
+            }
+            default: {
+                mp_printf(&mp_plat_print, " - Unknown\n");
+                break;
+            }
+        }
+    }
+}
+
+static void sdcard_cmd_go_idle_state(USDHC_Type *base) {
+    status_t status;
+    usdhc_command_t command = {
+        .index = 0,
+        .argument = 0UL,
+        .type = kCARD_CommandTypeNormal,
+        .responseType = kCARD_ResponseTypeNone,
+    };
+    usdhc_transfer_t transfer = {
+        .data = NULL,
+        .command = &command,
+    };
+    status = USDHC_TransferBlocking(base, NULL, &transfer);
+    sdcard_check_status(status, 0);
+}
+
+static uint32_t sdcard_cmd_oper_cond(USDHC_Type *base) {
+    status_t status;
+    usdhc_command_t command = {
+        .index = 8,
+        .argument = 0x000001AAU,       // 2.7-3.3V range and 0xAA check pattern
+        .type = kCARD_CommandTypeNormal,
+        .responseType = kCARD_ResponseTypeR7,
+    };
+    usdhc_transfer_t transfer = {
+        .data = NULL,
+        .command = &command,
+    };
+
+    status = USDHC_TransferBlocking(base, NULL, &transfer);
+    sdcard_check_status(status, 8);
+    return command.response[0];
+}
+
+static uint32_t sdcard_cmd_app_cmd(USDHC_Type *base) {
+    status_t status;
+    usdhc_command_t command = {
+        .index = 55,
+        .argument = 0UL,
+        .type = kCARD_CommandTypeNormal,
+        .responseType = kCARD_ResponseTypeR1,
+    };
+    usdhc_transfer_t transfer = {
+        .data = NULL,
+        .command = &command,
+    };
+
+    status = USDHC_TransferBlocking(base, NULL, &transfer);
+    sdcard_check_status(status, 55);
+    return command.response[0];
+}
+
+static uint32_t sdcard_cmd_sd_app_op_cond(USDHC_Type *base, uint32_t argument) {
+    status_t status;
+    usdhc_command_t command = {
+        .index = 41,
+        .argument = argument,
+        .type = kCARD_CommandTypeNormal,
+        .responseType = kCARD_ResponseTypeR3,
+    };
+
+    usdhc_transfer_t transfer = {
+        .data = NULL,
+        .command = &command,
+    };
+
+    status = USDHC_TransferBlocking(base, NULL, &transfer);
+    sdcard_check_status(status, 41);
+    return command.response[0];
+}
+
+static cid_t sdcard_cmd_all_send_cid(USDHC_Type *base) {
+    status_t status;
+    cid_t cid;
+    usdhc_command_t command = {
+        .index = 2,
+        .argument = 0UL,
+        .type = kCARD_CommandTypeNormal,
+        .responseType = kCARD_ResponseTypeR2,
+    };
+    usdhc_transfer_t transfer = {
+        .data = NULL,
+        .command = &command,
+    };
+
+    status = USDHC_TransferBlocking(base, NULL, &transfer);
+    sdcard_check_status(status, 2);
+    // --
+    cid.cid[0] = command.response[0];
+    cid.cid[1] = command.response[1];
+    cid.cid[2] = command.response[2];
+    cid.cid[3] = command.response[3];
+    // --
+    return cid;
+}
+
+static cid_t sdcard_cmd_send_cid(USDHC_Type *base, uint32_t rca) {
+    status_t status;
+    cid_t cid;
+    usdhc_command_t command = {
+        .index = 10,
+        .argument = (rca << 16),
+        .type = kCARD_CommandTypeNormal,
+        .responseType = kCARD_ResponseTypeR2,
+    };
+    usdhc_transfer_t transfer = {
+        .data = NULL,
+        .command = &command,
+    };
+
+    status = USDHC_TransferBlocking(base, NULL, &transfer);
+    sdcard_check_status(status, 10);
+    // --
+    cid.cid[0] = command.response[0];
+    cid.cid[1] = command.response[1];
+    cid.cid[2] = command.response[2];
+    cid.cid[3] = command.response[3];
+    // --
+    return cid;
+}
+
+static uint32_t sdcard_cmd_set_rel_add(USDHC_Type *base) {
+    status_t status;
+    usdhc_command_t command = {
+        .index = 3,
+        .argument = 0UL,
+        .type = kCARD_CommandTypeNormal,
+        .responseType = kCARD_ResponseTypeR6,
+    };
+    usdhc_transfer_t transfer = {
+        .data = NULL,
+        .command = &command,
+    };
+
+    status = USDHC_TransferBlocking(base, NULL, &transfer);
+    sdcard_check_status(status, 3);
+    return 0xFFFFFFFF & (command.response[0] >> 16);
+}
+
+static csd_t sdcard_cmd_send_csd(USDHC_Type *base, uint32_t rca) {
+    status_t status;
+    csd_t csd;
+    usdhc_command_t command = {
+        .index = 9,
+        .argument = (rca << 16),
+        .type = kCARD_CommandTypeNormal,
+        .responseType = kCARD_ResponseTypeR2,
+    };
+    usdhc_transfer_t transfer = {
+        .data = NULL,
+        .command = &command,
+    };
+
+    status = USDHC_TransferBlocking(base, NULL, &transfer);
+    sdcard_check_status(status, 9);
+    // --
+    csd.csd[0] = command.response[0];
+    csd.csd[1] = command.response[1];
+    csd.csd[2] = command.response[2];
+    csd.csd[3] = command.response[3];
+    // --
+    return csd;
+}
+
+
+static void sdcard_transfer_command(USDHC_Type *base, usdhc_command_t *command) {
+    status_t status;
+    usdhc_transfer_t transfer = {
+        .data = NULL,
+        .command = command,
+    };
+    // ---
+    status = USDHC_TransferBlocking(base, NULL, &transfer);
+
+    if (status != kStatus_Success) {
+        mp_printf(&mp_plat_print, "Command %d failed!", command->index);
+        switch (status)
+        {
+            case (kStatus_USDHC_BusyTransferring): {
+                mp_printf(&mp_plat_print, " - BusyTransferring (%d)\n", status);
+                break;
+            }
+            case (kStatus_USDHC_PrepareAdmaDescriptorFailed): {
+                mp_printf(&mp_plat_print, " - PrepareAdmaDescriptorFailed (%d)\n", status);
+                break;
+            }
+            case (kStatus_USDHC_SendCommandFailed): {
+                mp_printf(&mp_plat_print, " - SendCommandFailed (%d)\n", status);
+                break;
+            }
+            case (kStatus_USDHC_TransferDataFailed): {
+                mp_printf(&mp_plat_print, " - TransferDataFailed (%d)\n", status);
+                break;
+            }
+            case (kStatus_USDHC_DMADataAddrNotAlign): {
+                mp_printf(&mp_plat_print, " - DMADataAddrNotAlign (%d)\n", status);
+                break;
+            }
+            case (kStatus_USDHC_ReTuningRequest): {
+                mp_printf(&mp_plat_print, " - ReTuningRequest (%d)\n", status);
+                break;
+            }
+            case (kStatus_USDHC_TuningError): {
+                mp_printf(&mp_plat_print, " - TuningError (%d)\n", status);
+                break;
+            }
+            case (kStatus_USDHC_NotSupport): {
+                mp_printf(&mp_plat_print, " - NotSupport (%d)\n", status);
+                break;
+            }
+            default: {
+                mp_printf(&mp_plat_print, " - Unknown\n");
+                break;
+            }
+        }
+    } else {
+        mp_printf(&mp_plat_print, "Command %d succeeded!\n", command->index);
+
+        switch (command->responseType)
+        {
+            case (kCARD_ResponseTypeR1): {
+                mp_printf(&mp_plat_print, "\t0x%08X - Card Status\n", command->response[0]);
+                break;
+            }
+            case (kCARD_ResponseTypeR1b): {
+                mp_printf(&mp_plat_print, "\t0x%08X - Card Status for Auto CMD12\n", command->response[3]);
+                break;
+            }
+
+            case (kCARD_ResponseTypeR2): {
+                mp_printf(&mp_plat_print, "\tCID/CSD\n");
+                mp_printf(&mp_plat_print, "\t0x%08X\n", command->response[0]);
+                mp_printf(&mp_plat_print, "\t0x%08X\n", command->response[1]);
+                mp_printf(&mp_plat_print, "\t0x%08X\n", command->response[2]);
+                mp_printf(&mp_plat_print, "\t0x%08X\n", command->response[3]);
+                break;
+
+            }
+            case (kCARD_ResponseTypeR3): {
+                mp_printf(&mp_plat_print, "\t0x%08X - OCR\n", command->response[0]);
+                break;
+            }
+            default: {
+                // do nothing;
+                break;
+            }
+        }
+    }
+}
+
+
+static void sdcard_print_cid(cid_t cid) {
+    mp_printf(&mp_plat_print, "\tManufacturer ID\n");
+    mp_printf(&mp_plat_print, "\tOEM/Application ID\n");
+    mp_printf(&mp_plat_print, "\tProduct name: %s\n");
+    mp_printf(&mp_plat_print, "\tProduct revision: 0x%02X\n", (cid.cid[1] >> 24));
+    mp_printf(&mp_plat_print, "\tProduct serial number: 0x%08X\n", (cid.cid[1] << 16 | cid.cid[0] >> 16));
+    mp_printf(&mp_plat_print, "\tManufacturing date: %d.%d\n", ((cid.cid[0] >> 8) & 0xF), ((cid.cid[0] >> 12) & 0xFF) + 2000);
+}
 
 
 STATIC void machine_sdcard_init_helper(const mimxrt_sdcard_obj_t *self, const mp_arg_val_t *args) {
@@ -95,6 +418,8 @@ STATIC void machine_sdcard_init_helper(const mimxrt_sdcard_obj_t *self, const mp
     };
 
     USDHC_Init(self->sdcard, &config);
+    USDHC_SetSdClock(self->sdcard, 132000000UL, 300000UL);
+    USDHC_SetDataBusWidth(self->sdcard, kUSDHC_DataBusWidth1Bit);
 }
 
 
@@ -145,79 +470,65 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_3(machine_sdcard_writeblocks_obj, machine_sdcard_
 
 // ioctl(op, arg)
 STATIC mp_obj_t machine_sdcard_ioctl(mp_obj_t self_in, mp_obj_t cmd_in, mp_obj_t arg_in) {
-    const mimxrt_sdcard_obj_t *self = MP_OBJ_TO_PTR(self_in);
+    mimxrt_sdcard_obj_t *self = MP_OBJ_TO_PTR(self_in);
     mp_int_t cmd = mp_obj_get_int(cmd_in);
-
-
-    usdhc_transfer_t transfer;
-    usdhc_command_t command;
-
-
+    // ---
+    bool valid_voltage = false;
+    uint32_t count = 0UL;
+    uint32_t response;
+    // ---
     switch (cmd) {
         case MP_BLOCKDEV_IOCTL_INIT:
-            transfer.data = NULL;
-            transfer.command = &command;
-
-            // Enable card insertion interrupt
-            self->sdcard->INT_STATUS_EN |= USDHC_INT_STATUS_EN_CINSSEN(1);
-
-            // Wait for card insertion
-            // while(!(self->sdcard->INT_STATUS & USDHC_INT_STATUS_CINS(1))) {};
-            self->sdcard->INT_STATUS_EN &= ~USDHC_INT_STATUS_EN_CINSSEN(1);
-
             // Reset Card
-            self->sdcard->SYS_CTRL |= USDHC_SYS_CTRL_RSTA(1);
-            self->sdcard->SYS_CTRL |= USDHC_SYS_CTRL_INITA(1);
+            if (USDHC_Reset(self->sdcard, (USDHC_SYS_CTRL_RSTA_MASK | USDHC_SYS_CTRL_RSTC_MASK | USDHC_SYS_CTRL_RSTD_MASK), 2048) == false) {
+                mp_printf(&mp_plat_print, "USDHC_Reset timed out!\n");
+            } else {
+                mp_printf(&mp_plat_print, "USDHC_Reset performed\n");
+            }
 
-            command.index = 0;  // CMD0 - GO_IDLE_STATE
-            command.argument = 0UL;
-            command.type = kCARD_CommandTypeNormal;
-            command.responseType = kCARD_ResponseTypeNone;
-            USDHC_TransferBlocking(self->sdcard, NULL, &transfer);
-            mp_printf(&mp_plat_print, "SDCard - Command 0 issued - GO_IDLE_STATE");
+            if (USDHC_SetCardActive(self->sdcard, 4096) == false) { // TODO: remove timeout check or change to calculated value based on CLK frequency
+                mp_printf(&mp_plat_print, "USDHC_SetCardActive timed out!\n");
+            } else {
+                mp_printf(&mp_plat_print, "USDHC_SetCardActive performed\n");
+            }
+
+
+            sdcard_cmd_go_idle_state(self->sdcard);
+            sdcard_cmd_oper_cond(self->sdcard);
 
             // Perform voltage validation
-            command.index = 55; // CMD55 - APP_CMD
-            command.argument = 0UL;
-            command.type = kCARD_CommandTypeNormal;
-            command.responseType = kCARD_ResponseTypeR1;
-            USDHC_TransferBlocking(self->sdcard, NULL, &transfer);
-            mp_printf(&mp_plat_print, "SDCard - Command 55 issued - APP_CMD");
+            while ((count < SDMMC_MAX_VOLT_TRIAL) && (valid_voltage == 0U)) {
+                sdcard_cmd_app_cmd(self->sdcard);
+                response = sdcard_cmd_sd_app_op_cond(self->sdcard, (uint32_t)(SDMMC_VOLTAGE_WINDOW_SD | SDMMC_HIGH_CAPACITY | SD_SWITCH_1_8V_CAPACITY));
 
-            command.index = 41; // ACMD41 - SD_APP_OP_COND
-            command.argument = 0UL;
-            command.type = kCARD_CommandTypeNormal;
-            command.responseType = kCARD_ResponseTypeR3;
-            USDHC_TransferBlocking(self->sdcard, NULL, &transfer);
-            mp_printf(&mp_plat_print, "SDCard - Command 41 issued - SD_APP_OP_COND");
-            mp_printf(&mp_plat_print, "\tR[0] - %#04X", command.response[0]);
-            mp_printf(&mp_plat_print, "\tR[1] - %#04X", command.response[1]);
-            mp_printf(&mp_plat_print, "\tR[2] - %#04X", command.response[2]);
-            mp_printf(&mp_plat_print, "\tR[3] - %#04X", command.response[3]);
+                /* Get operating voltage*/
+                valid_voltage = (((response >> 31U) == 1U) ? true : false);
+                count++;
 
-            command.index = 2; // CMD2 - ALL_SEND_CID
-            command.argument = 0UL;
-            command.type = kCARD_CommandTypeNormal;
-            command.responseType = kCARD_ResponseTypeR2;
-            USDHC_TransferBlocking(self->sdcard, NULL, &transfer);
-            USDHC_TransferBlocking(self->sdcard, NULL, &transfer);
-            mp_printf(&mp_plat_print, "SDCard - Command 2 issued - ALL_SEND_CID");
-            mp_printf(&mp_plat_print, "\tR[0] - %#04X", command.response[0]);
-            mp_printf(&mp_plat_print, "\tR[1] - %#04X", command.response[1]);
-            mp_printf(&mp_plat_print, "\tR[2] - %#04X", command.response[2]);
-            mp_printf(&mp_plat_print, "\tR[3] - %#04X", command.response[3]);
+                if (!(count % 10000)) {
+                    mp_printf(&mp_plat_print, "\t%d Voltage validation cycles executed\n", count);
+                }
+            }
 
-            command.index = 3; // CMD3 - SEND_RELATIVE_ADDR
-            command.argument = 0UL;
-            command.type = kCARD_CommandTypeNormal;
-            command.responseType = kCARD_ResponseTypeR1;
-            USDHC_TransferBlocking(self->sdcard, NULL, &transfer);
-            USDHC_TransferBlocking(self->sdcard, NULL, &transfer);
-            mp_printf(&mp_plat_print, "SDCard - Command 3 issued - SEND_RELATIVE_ADDR");
-            mp_printf(&mp_plat_print, "\tR[0] - %#04X", command.response[0]);
-            mp_printf(&mp_plat_print, "\tR[1] - %#04X", command.response[1]);
-            mp_printf(&mp_plat_print, "\tR[2] - %#04X", command.response[2]);
-            mp_printf(&mp_plat_print, "\tR[3] - %#04X", command.response[3]);
+            if (count >= SDMMC_MAX_VOLT_TRIAL) {
+                mp_printf(&mp_plat_print, "Invalid voltage range!\n");
+            } else {
+                mp_printf(&mp_plat_print, "Voltage validation performed (%d cycles)\n", count);
+            }
+
+            cid_t cid_all = sdcard_cmd_all_send_cid(self->sdcard);
+            mp_printf(&mp_plat_print, "CID of all cards received:\n\t[3]0x%08X\n\t[2]0x%08X\n\t[1]0x%08X\n\t[0]0x%08X\n", cid_all.cid[3],cid_all.cid[2],cid_all.cid[1],cid_all.cid[0]);
+            sdcard_print_cid(cid_all);
+
+            uint32_t rca = sdcard_cmd_set_rel_add(self->sdcard);
+            mp_printf(&mp_plat_print, "RCA received - 0x%04X\n", rca);
+
+            cid_t cid = sdcard_cmd_send_cid(self->sdcard, rca);
+            mp_printf(&mp_plat_print, "CID of RCA referenced card received:\n\t[3]0x%08X\n\t[2]0x%08X\n\t[1]0x%08X\n\t[0]0x%08X\n", cid.cid[3],cid.cid[2],cid.cid[1],cid.cid[0]);
+            sdcard_print_cid(cid);
+
+            csd_t csd = sdcard_cmd_send_csd(self->sdcard, rca);
+            mp_printf(&mp_plat_print, "CSD of RCA referenced card received:\n\t[3]0x%08X\n\t[2]0x%08X\n\t[1]0x%08X\n\t[0]0x%08X\n", csd.csd[3],csd.csd[2],csd.csd[1],csd.csd[0]);
         case MP_BLOCKDEV_IOCTL_DEINIT:
         case MP_BLOCKDEV_IOCTL_SYNC:
             return MP_OBJ_NEW_SMALL_INT(0);
