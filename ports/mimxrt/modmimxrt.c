e996bfe159adc6079c3928213831db13ca1aea8e
robert-hh <robert@hammelrath.com>
2021-06-18 22:06:53 +0200

    mimxrt: Add low level flash methods to module mimxrt.
    -  flash_read
    -  flash_write
    -  flash_erase
    -  flash_size
    -  flash_user_start

--- ports/mimxrt/modmimxrt.c
+++ ports/mimxrt/modmimxrt.c
@@ -26,10 +26,74 @@
 
 #include "py/runtime.h"
 #include "modmimxrt.h"
+#include "py/mperrno.h"
+#include "hal/flexspi_nor_flash.h"
+
+extern status_t flash_erase_block(uint32_t erase_addr);
+extern status_t flash_write_block(uint32_t dest_addr, const uint8_t *src, uint32_t length);
+extern uint8_t __vfs_start;
+extern uint8_t __vfs_end;
+extern uint8_t __flash_start;
+// BOARD_FLASH_SIZE is defined in mpconfigport.h
+#define SECTOR_SIZE_BYTES (qspiflash_config.sectorSize)
+#define PAGE_SIZE_BYTES (qspiflash_config.pageSize)
+
+
+STATIC mp_obj_t mimxrt_flash_read(mp_obj_t offset_in, mp_obj_t buf_in) {
+    mp_int_t offset = mp_obj_get_int(offset_in);
+    mp_buffer_info_t bufinfo;
+    mp_get_buffer_raise(buf_in, &bufinfo, MP_BUFFER_WRITE);
+    memcpy(bufinfo.buf, (uint8_t *)(FlexSPI_AMBA_BASE + offset), bufinfo.len);
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_2(mimxrt_flash_read_obj, mimxrt_flash_read);
+
+STATIC mp_obj_t mimxrt_flash_write(mp_obj_t offset_in, mp_obj_t buf_in) {
+    mp_int_t offset = mp_obj_get_int(offset_in);
+    mp_buffer_info_t bufinfo;
+    mp_get_buffer_raise(buf_in, &bufinfo, MP_BUFFER_READ);
+    // address and length must be a multiple of the page size
+    if (offset % PAGE_SIZE_BYTES || bufinfo.len % PAGE_SIZE_BYTES) {
+        mp_raise_OSError(MP_EINVAL);
+    }
+    status_t status = flash_write_block(offset, bufinfo.buf, bufinfo.len);
+    if (status != kStatus_Success) {
+        mp_raise_OSError(MP_EIO);
+    }
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_2(mimxrt_flash_write_obj, mimxrt_flash_write);
+
+STATIC mp_obj_t mimxrt_flash_erase(mp_obj_t sector_in) {
+    mp_int_t sector = mp_obj_get_int(sector_in);
+    status_t status = flash_erase_block(sector * SECTOR_SIZE_BYTES);
+    if (status != kStatus_Success) {
+        mp_raise_OSError(MP_EIO);
+    }
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(mimxrt_flash_erase_obj, mimxrt_flash_erase);
+
+STATIC mp_obj_t mimxrt_flash_size(void) {
+    return mp_obj_new_int_from_uint(BOARD_FLASH_SIZE);
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_0(mimxrt_flash_size_obj, mimxrt_flash_size);
+
+STATIC mp_obj_t mimxrt_flash_user_start(void) {
+    return mp_obj_new_int_from_uint((uint32_t)&__vfs_start - FlexSPI_AMBA_BASE);
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_0(mimxrt_flash_user_start_obj, mimxrt_flash_user_start);
+
 
 STATIC const mp_rom_map_elem_t mimxrt_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR___name__),            MP_ROM_QSTR(MP_QSTR_mimxrt) },
     { MP_ROM_QSTR(MP_QSTR_Flash),               MP_ROM_PTR(&mimxrt_flash_type) },
+
+    { MP_ROM_QSTR(MP_QSTR_flash_read), MP_ROM_PTR(&mimxrt_flash_read_obj) },
+    { MP_ROM_QSTR(MP_QSTR_flash_write), MP_ROM_PTR(&mimxrt_flash_write_obj) },
+    { MP_ROM_QSTR(MP_QSTR_flash_erase), MP_ROM_PTR(&mimxrt_flash_erase_obj) },
+    { MP_ROM_QSTR(MP_QSTR_flash_size), MP_ROM_PTR(&mimxrt_flash_size_obj) },
+    { MP_ROM_QSTR(MP_QSTR_flash_user_start), MP_ROM_PTR(&mimxrt_flash_user_start_obj) },
 };
 STATIC MP_DEFINE_CONST_DICT(mimxrt_module_globals, mimxrt_module_globals_table);
 
