028660f6824ef8f0bea659c5e06188d79b666d42
Philipp Ebensberger <philipp.ebensberger@3bricks-software.de>
2021-08-27 17:17:49 +0200

    mimxrt/1170_evk: Fixed tiny usb.

--- ports/mimxrt/boards/TEENSY41/mpconfigboard.mk
+++ ports/mimxrt/boards/TEENSY41/mpconfigboard.mk
@@ -8,9 +8,10 @@ BOARD_FLASH_SIZE ?= 0x800000  # 8MB
 BOARD_FLASH_RESERVED ?= 0x1000  # 4KB
 
 CFLAGS += -DCPU_HEADER_H='<$(MCU_SERIES).h>' \
+			-DCFG_TUSB_MCU=OPT_MCU_MIMXRT10XX
 
 SRC_C += \
-	hal/flexspi_nor_flash.c \
+	hal/flexspi_nor_flash.c
 
 deploy: $(BUILD)/firmware.hex
 	teensy_loader_cli --mcu=imxrt1062 -v -w $<
