6afd5905c50b9741a49a560827730ac96336f03d
robert-hh <robert@hammelrath.com>
2022-05-23 17:21:09 +0200

    w60x: A series of changes to follow the master branch.
    - w60x: A series of changes to follow the master branch.
      - Rename thread_t to mp_thread_t.
        This adds the `mp_` prefix to the `thread_t` type.  The name `thread_t`
        conflicts with the same in `mach/mach_types.h` on macOS.
      - Remove vfs defines from mpconfigport.h.
      - Module registration
      - separate include of extmod.mk
      - Fix a typo in Makefile.
      - Use MP_REGISTER_ROOT_POINTER().
      - Always include debug information in the ELF file.
      - Fix an edit error in mpthreadport.c.
      - Fix after rebase for changed vstr interface.
      - Adapt for PR8813 - Optimise mp_obj_type_t storage.
      - Fix manifest.py.
      - Update after rebase. Changed integration of oofatfs and onewire.
    
    - w600: Do not import upysh and pye on boot.
    
      upysh does not work well together with Thonny. You can still use
      them if you do not use Thonny by importing then at the REPL prompt with:
    
      from upysh import *
      from pye import pye
    
      For freezing, use the micropython/lib/... path for upysh and do not
      freeze pye. It can still be put into the file system as cross-compiled
      code to speed up loading.
    
    - w60x: Do not execute main.py if boot.py fails.
      Making it identical to the other ports.
    
    - w60x: Add time.gmtime() as alias to time.localtime().
      Allowing the lib version of ntptime.py to be used.
    
    - w60x: Fix the include path set for mbedtls.
    - w60x: Update README.md.
      Text changes mostly made by @LexxM3.
    
    - w60x: Track master changes.
      - add network-bundle
      - rename gchelper file.
    
    - w60x: Use the defaults for MICROPY_PY_THREAD_GIL ...
      Derived from the setting of MICROPY_PY_THREAD.
      And allow MICROPY_PY_THREAD=x to be set at the make command.
    
    Signed-off-by: robert-hh <robert@hammelrath.com>

--- ports/w60x/machine/machine_rtc.c
+++ ports/w60x/machine/machine_rtc.c
@@ -222,10 +222,10 @@ STATIC const mp_rom_map_elem_t pyb_rtc_locals_dict_table[] = {
 };
 STATIC MP_DEFINE_CONST_DICT(pyb_rtc_locals_dict, pyb_rtc_locals_dict_table);
 
-const mp_obj_type_t machine_rtc_type = {
-    { &mp_type_type },
-    .name = MP_QSTR_RTC,
-    .make_new = machine_rtc_make_new,
-    .locals_dict = (mp_obj_t) &pyb_rtc_locals_dict,
-};
-
+MP_DEFINE_CONST_OBJ_TYPE(
+    machine_rtc_type,
+    MP_QSTR_RTC,
+    MP_TYPE_FLAG_NONE,
+    make_new, machine_rtc_make_new,
+    locals_dict, &pyb_rtc_locals_dict
+    );
