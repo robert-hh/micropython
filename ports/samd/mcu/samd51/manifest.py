879f341714dfec5ad484fece29a135939138ebae
robert-hh <robert@hammelrath.com>
2023-04-20 15:21:43 +0200

    samd/BLE: Change main.c, pendsv.h, mpconfigxxx.h and makefiles for BLE.
    Signed-off-by: robert-hh <robert@hammelrath.com>

--- ports/samd/mcu/samd51/manifest.py
+++ ports/samd/mcu/samd51/manifest.py
@@ -1,2 +1,13 @@
 include("$(PORT_DIR)/boards/manifest.py")
 include("$(MPY_DIR)/extmod/asyncio")
+require("onewire")
+require("ds18x20")
+require("dht")
+
+include(
+    "$(MPY_LIB_DIR)/micropython/bluetooth/aioble/manifest.py",
+    client=True,
+    central=True,
+    l2cap=True,
+    security=True,
+)
