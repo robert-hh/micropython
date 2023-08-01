da48ed605b35bd57e79612cb3eb12e17f7ff91d7
robert-hh <robert@hammelrath.com>
2023-08-01 21:44:19 +0200

    samd: Use esp-hosted for WiFi and BLE.
    Both WiFi and BLE work. Code size increase ~250k.
    BLE should be used with the modified mynewt-nimble lib avoiding a
    lock-up in error states. To be set manually.
    
    Signed-off-by: robert-hh <robert@hammelrath.com>

--- ports/samd/boards/SPARKFUN_SAMD51_THING_PLUS/mpconfigboard.h
+++ ports/samd/boards/SPARKFUN_SAMD51_THING_PLUS/mpconfigboard.h
@@ -13,3 +13,31 @@
 // the board's external flash.
 #define MICROPY_HW_SPIFLASH         (1)
 #define MICROPY_HW_SPIFLASH_ID      (0)
+
+// Bluetooth config.
+#define MICROPY_HW_BLE_UART_ID          (2)
+#define MICROPY_HW_BLE_UART_RX          (13)
+#define MICROPY_HW_BLE_UART_TX          (12)
+// MOSI
+#define MICROPY_HW_BLE_UART_RTS         (32 + 12)
+#define MICROPY_HW_BLE_UART_BAUDRATE    (460800)
+#define MICROPY_HW_BLE_UART_BAUDRATE_SECONDARY (460800)
+
+// WiFi config.
+#define MICROPY_HW_WIFI_SPI_ID          (4)
+#define MICROPY_HW_WIFI_SPI_BAUDRATE    (8 * 1000 * 1000)
+
+#define MICROPY_HW_WIFI_SPI_CS          (17)
+#define MICROPY_HW_WIFI_SPI_MOSI        (32 + 12)
+#define MICROPY_HW_WIFI_SPI_MISO        (32 + 11)
+#define MICROPY_HW_WIFI_SPI_SCK         (32 + 13)
+
+#define MICROPY_HW_WIFI_DATAREADY       (18)
+#define MICROPY_HW_WIFI_HANDSHAKE       (16)
+#define MICROPY_HW_WIFI_IRQ_PIN         (MICROPY_HW_WIFI_DATAREADY)
+
+// ESP hosted control pins
+#define MICROPY_HW_ESP_HOSTED_RESET     (19)
+#define MICROPY_HW_ESP_HOSTED_GPIO0     (18)
+
+#define MICROPY_HW_ESP_HOSTED_SHARED_PINS (1)
