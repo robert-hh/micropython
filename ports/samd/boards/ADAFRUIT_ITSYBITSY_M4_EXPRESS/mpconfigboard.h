9e512a87dd98e8cb729b8da0a9eefc6aef4b0448
robert-hh <robert@hammelrath.com>
2023-09-17 20:54:49 +0200

    samd: Move the configuration, add Feather M4, Metro M4, Itsy Bitsy M4.
    Most of the configuration can be moved to mcu/samd/mpconfigmcu.mk.
    Some include file definitions are already in nimble.mk.
    
    For Feather M4, Metro M4 and Itsy Bitsy M4 the code fills up almost all
    of the flash. Maybe not the best option. Using the NINA firmware needs
    less flash, but the BLE performance is worse.
    
    Signed-off-by: robert-hh <robert@hammelrath.com>

--- ports/samd/boards/ADAFRUIT_ITSYBITSY_M4_EXPRESS/mpconfigboard.h
+++ ports/samd/boards/ADAFRUIT_ITSYBITSY_M4_EXPRESS/mpconfigboard.h
@@ -4,3 +4,32 @@
 #define MICROPY_HW_DFLL_USB_SYNC    (1)
 
 #define MICROPY_HW_QSPIFLASH        GD25Q16C
+
+// Bluetooth config.
+#define MICROPY_HW_BLE_UART_ID          (3)
+#define MICROPY_HW_BLE_UART_RX          (16)
+#define MICROPY_HW_BLE_UART_TX          (17)
+// MOSI
+#define MICROPY_HW_BLE_UART_RTS         (0)
+#define MICROPY_HW_BLE_UART_BAUDRATE    (115200)
+#define MICROPY_HW_BLE_UART_BAUDRATE_SECONDARY (460800)
+
+// WiFi config.
+
+#define MICROPY_HW_WIFI_SPI_ID          (1)
+#define MICROPY_HW_WIFI_SPI_BAUDRATE    (8000000)
+
+#define MICROPY_HW_WIFI_SPI_CS          (22)
+#define MICROPY_HW_WIFI_SPI_MOSI        (0)
+#define MICROPY_HW_WIFI_SPI_MISO        (55)
+#define MICROPY_HW_WIFI_SPI_SCK         (1)
+
+#define MICROPY_HW_WIFI_DATAREADY       (20)
+#define MICROPY_HW_WIFI_HANDSHAKE       (21)
+#define MICROPY_HW_WIFI_IRQ_PIN         (MICROPY_HW_WIFI_DATAREADY)
+
+// ESP hosted control pins
+#define MICROPY_HW_ESP_HOSTED_RESET     (23)
+#define MICROPY_HW_ESP_HOSTED_GPIO0     (20)
+
+#define MICROPY_HW_ESP_HOSTED_SHARED_PINS (1)
