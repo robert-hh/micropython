0d92f172be81f2f70523419662e2364219ae77f0
robert-hh <robert@hammelrath.com>
2023-09-23 10:24:16 +0200

    samd/WLAN: Support external NINA10 modules for WLAN.
    Using the network.WLAN module with Pin settings defined in
    mpconfigboard.h.
    
    To allow for port specific WiFi configurations, the functions in
    drivers/ninaw10/nina_wifi_bsp.c are declared as MP_WEAK.
    
    Signed-off-by: robert-hh <robert@hammelrath.com>

--- ports/samd/main.c
+++ ports/samd/main.c
@@ -31,6 +31,8 @@
 #include "py/mphal.h"
 #include "py/stackctrl.h"
 #include "extmod/modmachine.h"
+// #include "extmod/modbluetooth.h"
+#include "extmod/modnetwork.h"
 #include "shared/readline/readline.h"
 #include "shared/runtime/gchelper.h"
 #include "shared/runtime/pyexec.h"
@@ -55,6 +57,12 @@ void samd_main(void) {
 
         // Initialise sub-systems.
         readline_init0();
+        #if MICROPY_PY_BLUETOOTH
+        // mp_bluetooth_hci_init();
+        #endif
+        #if MICROPY_PY_NETWORK
+        mod_network_init();
+        #endif
 
         // Execute _boot.py to set up the filesystem.
         pyexec_frozen_module("_boot.py", false);
@@ -90,6 +98,12 @@ void samd_main(void) {
 
     soft_reset_exit:
         mp_printf(MP_PYTHON_PRINTER, "MPY: soft reboot\n");
+        #if MICROPY_PY_NETWORK
+        mod_network_deinit();
+        #endif
+        #if MICROPY_PY_BLUETOOTH
+        // mp_bluetooth_deinit();
+        #endif
         #if MICROPY_PY_MACHINE_ADC
         adc_deinit_all();
         #endif
