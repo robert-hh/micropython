0809ecf2475e540683f759213c35d3b29e018863
robert-hh <robert@hammelrath.com>
2023-10-16 20:58:39 +0200

    w60x: Make machine.adc() optional.
    The ADC pin is not accessible at most boards. So it's disabled by
    default and enabled only for the GENERIC build.
    
    Signed-off-by: robert-hh <robert@hammelrath.com>

--- ports/w60x/include/modmachine.h
+++ ports/w60x/include/modmachine.h
@@ -8,7 +8,6 @@
 extern const mp_obj_type_t machine_timer_type;
 extern const mp_obj_type_t machine_wdt_type;
 extern const mp_obj_type_t machine_pin_type;
-extern const mp_obj_type_t machine_adc_type;
 extern const mp_obj_type_t machine_pwm_type;
 extern const mp_obj_type_t machine_hw_spi_type;
 extern const mp_obj_type_t machine_spi_type;
@@ -16,5 +15,8 @@ extern const mp_obj_type_t machine_hw_i2c_type;
 extern const mp_obj_type_t machine_spi_type;
 extern const mp_obj_type_t machine_uart_type;
 extern const mp_obj_type_t machine_rtc_type;
+#if MICROPY_PY_MACHINE_ADC
+extern const mp_obj_type_t machine_adc_type;
+#endif
 
 #endif // MICROPY_INCLUDED_W600_MODMACHINE_H
