4ae46a7fa466b2518ef59313c79d3ef62eea1234
robert-hh <robert@hammelrath.com>
2023-10-26 22:08:23 +0200

    w60x: Use the extended extmod/modmachine.h file.
    Signed-off-by: robert-hh <robert@hammelrath.com>

--- ports/w60x/machine/machine_spi.c
+++ ports/w60x/machine/machine_spi.c
@@ -35,7 +35,6 @@
 #include "py/runtime.h"
 #include "py/stream.h"
 #include "py/mphal.h"
-#include "extmod/machine_spi.h"
 #include "extmod/modmachine.h"
 #include "modmachine.h"
 #include "wm_hostspi.h"
