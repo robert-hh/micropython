b19766a5942f16bd623fe16b966b2b98033fad90
robert-hh <robert@hammelrath.com>
2023-10-26 22:08:23 +0200

    w60x: Use the extended extmod/modmachine.h file.
    Signed-off-by: robert-hh <robert@hammelrath.com>

--- ports/w60x/include/modmachine.h
+++ ports/w60x/include/modmachine.h
@@ -5,9 +5,7 @@
 
 #define W600_YEAR_BASE 2000
 
-extern const mp_obj_type_t machine_pin_type;
 extern const mp_obj_type_t machine_hw_spi_type;
 extern const mp_obj_type_t machine_hw_i2c_type;
-extern const mp_obj_type_t machine_rtc_type;
 
 #endif // MICROPY_INCLUDED_W600_MODMACHINE_H
