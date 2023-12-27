53fa92d31c6ac35880e3843a021e716af7440ad0
robert-hh <robert@hammelrath.com>
2023-12-27 15:08:24 +0100

    w60x: Update to V1.22.0.
    Including changes to follow the main line:
    
    - move dupterm notify code to extmod.
    - return number of characters written by write to stdout.
    
    Signed-off-by: robert-hh <robert@hammelrath.com>

--- ports/w60x/modules/modos.c
+++ ports/w60x/modules/modos.c
@@ -80,18 +80,3 @@ STATIC mp_obj_t mp_os_urandom(mp_obj_t num) {
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(mp_os_urandom_obj, mp_os_urandom);
 #endif
-
-#if MICROPY_PY_OS_DUPTERM
-STATIC mp_obj_t mp_os_dupterm_notify(mp_obj_t obj_in) {
-    (void)obj_in;
-    for (;;) {
-        int c = mp_os_dupterm_rx_chr();
-        if (c < 0) {
-            break;
-        }
-        ringbuf_put(&stdin_ringbuf, c);
-    }
-    return mp_const_none;
-}
-STATIC MP_DEFINE_CONST_FUN_OBJ_1(mp_os_dupterm_notify_obj, mp_os_dupterm_notify);
-#endif
