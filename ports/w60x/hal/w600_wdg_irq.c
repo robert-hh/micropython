0ff9ba82f5b4ddf01a56c66e22a65682b5dbb1ef
robert-hh <robert@hammelrath.com>
2024-04-15 16:47:53 +0200

    w60x/mphalport.c: Improve ticks_us() and the GPIO low level IRQ handler.
    - The execution time of ticks_us() is now fix to about 2 µs, achieved by
      changing the division by 40 method. It used to be 2-12 µs.
    - The latency of the GPIO IRQ is now almost constant 6µs. It used to
      be between 2 and 18µs depending on the GPIO port. For that change,
      the file wm_gpio.c had to be moved from the WMSK into the hal folder.
    - Move the WDG counter IRQ and the readout of the counter to RAM.
      That way these functions are not subject to code cache misses.
    - Add double reading & compare for reading the 40MHz counter value.
      If the the first reading is larger than the second, the first
      reading as interrupted by a WDG interrupt, and value is corrected.
    
    The division by 40 method and the changes to wm_gpio.c for constant
    GPIO IRQ latency were provided by @rkompass.
    
    Signed-off-by: robert-hh <robert@hammelrath.com>

---
+++ ports/w60x/hal/w600_wdg_irq.c
@@ -0,0 +1,46 @@
+/* Development of the code in this file was sponsored by Microbric Pty Ltd
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Damien P. George
+ * Copyright (c) 2024 Robert Hammelrath
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include "wm_osal.h"
+#include "wm_regs.h"
+#include "wm_watchdog.h"
+
+extern volatile uint64_t wdg_ticks_total;
+extern uint32_t wdg_ticks_reload_value;
+
+void WDG_IRQHandler(void *data) {
+    wdg_ticks_total += wdg_ticks_reload_value;
+    tls_reg_write32(HR_WDG_INT_CLR, 0x01);
+}
+
+uint64_t get_wdg_counter_value(void) {
+    // The below sequence of statements just expresses the importance of
+    // HR_WDG_CUR_VALUE being loaded before wdg_ticks_total. The compiler
+    // will do that anyhow, even if the WDG registe access is in the return
+    // statement's expression.
+    int32_t wdg_ticks_value_at_get = *(TLS_REG *)HR_WDG_CUR_VALUE;
+    return wdg_ticks_total + (uint64_t)(wdg_ticks_reload_value - wdg_ticks_value_at_get);
+}
