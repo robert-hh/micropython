52e737361c80dfa62a4e0a1496ae00fe61a47b96
robert-hh <robert@hammelrath.com>
2024-09-12 12:08:09 +0200

    samd: Rework board variant support using `mpconfigvariant_XXX.mk` files.
    That simplifies each of these files.
    
    Signed-off-by: robert-hh <robert@hammelrath.com>

---
+++ ports/samd/mcu/samd51/mpconfigvariant_WLAN_BLE.mk
@@ -0,0 +1,24 @@
+MICROPY_PY_NETWORK = 1
+CFLAGS += -DMICROPY_PY_NETWORK=1
+MICROPY_PY_NETWORK_ESP_HOSTED = 1
+MICROPY_PY_LWIP = 1
+
+ifeq ($(MICROPY_HW_CODESIZE), 1008K)
+    MICROPY_PY_SSL = 1
+    MICROPY_SSL_MBEDTLS = 1
+endif
+
+MICROPY_PY_BLUETOOTH = 1
+CFLAGS += -DMICROPY_PY_BLUETOOTH=$(MICROPY_PY_BLUETOOTH)
+MICROPY_BLUETOOTH_NIMBLE = 1
+MICROPY_BLUETOOTH_BTSTACK = 0
+
+SRC_C += \
+    esp_hosted_hal.c \
+    mpbthciport.c \
+    mpnimbleport.c \
+    mpnetworkport.c \
+    mbedtls/mbedtls_port.c
+
+INC += \
+    -Ilwip_inc
