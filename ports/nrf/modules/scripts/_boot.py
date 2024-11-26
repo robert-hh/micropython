9edaa4428f7727c47bbaad7443e4cbd642b39d82
robert-hh <robert@hammelrath.com>
2024-11-26 13:25:29 +0100

    nrf/main: Use frozen _boot.py to set up and mount the file system.
    Replacing a mix of C-Code and Python code. The mkfs part has been
    simplified to save code.
    
    Signed-off-by: robert-hh <robert@hammelrath.com>

---
+++ ports/nrf/modules/scripts/_boot.py
@@ -0,0 +1,34 @@
+import gc
+import vfs
+import sys
+import nrf
+import os
+
+try:
+    vfs.mount(vfs.VfsRom(vfs.rom_ioctl(1)), "/rom")
+    sys.path.insert(0, "/rom")
+except:
+    pass
+
+try:
+    bdev = nrf.Flash()
+    fs_type = (
+        vfs.VfsLfs2
+        if hasattr(vfs, "VfsLfs2")
+        else (vfs.VfsLfs1 if hasattr(vfs, "VfsLfs1") else vfs.VfsFAT)
+    )
+    vfs.mount(fs_type(bdev), "/flash")
+except:
+    try:
+        fs_type.mkfs(bdev)
+        vfs.mount(fs_type(bdev), "/flash")
+    except:
+        pass
+
+os.chdir("/flash")
+sys.path.append("/flash")
+sys.path.append("/flash/lib")
+
+del os, nrf, vfs, sys, bdev, fs_type
+gc.collect()
+del gc
